a75a9e25bde3376322eaffec9dcd2c0674620361
Murad Hossain <sliverboymurad@gmail.com>
2020-10-13 19:39:24 +0600

    carousel added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Contact from '../Contact/Contact';
 import FeedBacks from '../FeedBacks/FeedBacks/FeedBacks';
 import Header from '../Header/Header';
+import OurWork from '../OurWork/OurWork';
 import Partner from '../Partner/Partner';
 import Services from '../Services/Services/Services';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Header></Header>
             <Partner></Partner>
             <Services></Services>
+            <OurWork></OurWork>
             <FeedBacks></FeedBacks>
             <Contact></Contact>
         </div>
